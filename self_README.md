0dcdb8124a59d976f888aa8d10d2ff676ab87b18
Alif Abdullah <alifabdullah7040@gmail.com>
2025-06-22 07:06:11 -0400

    Tried out text generation

--- self_README.md
+++ self_README.md
@@ -4,4 +4,4 @@ conda activate huggingface-transformers
 
 [Your current point in the LLM course you are following](https://huggingface.co/learn/llm-course/chapter1/3)
 
-Currently on Now let’s see how to use a pipeline to generate some text.
+Currently on Using any model from the Hub in a pipeline.
--- transformer-models.py
+++ transformer-models.py
@@ -8,3 +8,11 @@ def zero_shot_classification_tryout(sentence : str, list_of_labels : List[str]):
 
 # Zero Shot Classification Tryout
 # print(zero_shot_classification_tryout("Why is the sky blue? Does that have any impact on global governance? What if the sky was red? Are starfish a type of crustacean or echinoderm?",["science","governance","policy","finance","unga","food"]))
+
+def text_generation(starting_sentence_fragment : str, num_return_sequences: int, max_length: int):
+  text_generator = transformers.pipeline("text-generation")
+  return text_generator(starting_sentence_fragment, num_return_sequences=num_return_sequences, max_length=max_length, max_new_tokens=None)
+
+
+# Text Generation Tryout
+print(text_generation("Todos de los hombres del rey lo", 2, 15))
